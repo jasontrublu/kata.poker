e667ddce2b98c3077454c12474afa1205948380c
Jason Peper <trublu@c-peper.de>
2017-03-25 22:32:03 +0000

    create interaction for input parser

--- src/main/groovy/Poker.groovy
+++ src/main/groovy/Poker.groovy
@@ -1,5 +1,8 @@
 class Poker {
+    InputParser inputParser
+
     def game(String input) {
+        inputParser.parse(input)
         return "White wins. - with high card: Ace"
     }
 }
--- src/test/groovy/PokerSpec.groovy
+++ src/test/groovy/PokerSpec.groovy
@@ -1,10 +1,23 @@
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class PokerSpec extends Specification {
-    def "nothing"() {
-        given:
-            def poker = new Poker()
+    InputParser parser = Mock(InputParser)
+    def poker = new Poker(inputParser: parser)
+
+    @Unroll
+    def "test that game with input '#input' returns '#output'"() {
         expect:
-            poker.game("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH") == "White wins. - with high card: Ace"
+            poker.game(input) == output
+        where:
+            input                                          | output
+            "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH" | "White wins. - with high card: Ace"
+    }
+
+    def "game send input into input parser"() {
+        when:
+            poker.game("input")
+        then:
+            1 * parser.parse("input")
     }
 }
