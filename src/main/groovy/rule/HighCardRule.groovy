a9720e6f7adb3ffeb70f3daa23808c556fd59d30
Jason Peper <trublu@c-peper.de>
2017-04-03 22:19:46 +0100

    extract method to get winner from two cards

--- src/main/groovy/rule/HighCardRule.groovy
+++ src/main/groovy/rule/HighCardRule.groovy
@@ -1,15 +1,30 @@
 package rule
 
+import model.Card
 import model.Hand
 import model.Result
 import model.Winner
 import service.HandComparator
 
 class HighCardRule implements HandComparator {
+    
     @Override
     Result compare(final Hand handWhite, final Hand handBlack) {
         def cardWhite = handWhite.cards.first()
         def cardBlack = handBlack.cards.first()
+        Result out = getWinner(cardWhite, cardBlack)
+        if (out != Result.TIE) {
+            return out
+        }
+        if (handWhite.cards.size() > 1 && handBlack.cards.size() > 1) {
+            cardWhite = handWhite.cards.get(1)
+            cardBlack = handBlack.cards.get(1)
+            out = getWinner(cardWhite, cardBlack)
+        }
+        return out
+    }
+
+    private static Result getWinner(Card cardWhite, Card cardBlack) {
         def comp = cardWhite <=> cardBlack
         if (comp < 0) {
             return new Result(Winner.BLACK, cardBlack.toString())
@@ -17,17 +32,6 @@ class HighCardRule implements HandComparator {
         if (comp > 0) {
             return new Result(Winner.WHITE, cardWhite.toString())
         }
-        if (handWhite.cards.size() > 1 && handBlack.cards.size() > 1 ) {
-            cardWhite = handWhite.cards.get(1)
-            cardBlack = handBlack.cards.get(1)
-            comp = cardWhite <=> cardBlack
-            if (comp < 0) {
-                return new Result(Winner.BLACK, cardBlack.toString())
-            }
-            if (comp > 0) {
-                return new Result(Winner.WHITE, cardWhite.toString())
-            }
-        }
-        return Result.TIE
+        Result.TIE
     }
 }
